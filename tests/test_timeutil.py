a5298a6056fdecb223a3d22e99a03340cc19c9e6
juacompe <juacompe@gmail.com>
2013-08-24 12:10:34 +0700

    - time_from_now in bracket is correct

--- tests/test_timeutil.py
+++ tests/test_timeutil.py
@@ -15,12 +15,14 @@ class TestTimeutil(TestCase):
         self.assertTrue("31 December 2013 at 10:00 PM" in result)
         
     def test_bracket_at_10pm(self):
+        now = datetime(2013, 8, 24, 11, 40)
         time = datetime(2013, 8, 24, 22, 0)
-        result = get_time(time)
+        result = get_time(time, now)
         self.assertTrue("(10 hours 20 minutes from now)" in result)
     
     def test_bracket_at_11pm(self):
+        now = datetime(2013, 8, 24, 11, 40)
         time = datetime(2013, 8, 24, 23, 0)
-        result = get_time(time)
+        result = get_time(time, now)
         self.assertTrue("(11 hours 20 minutes from now)" in result)
 
--- timeutil.py
+++ timeutil.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-def get_time(time):
+#def get_time(time, now=None):
+def get_time(time, now=None):
     time_str = time.strftime("%I:%M %p")
-    now = datetime.now()
+    now = now or datetime.now()
     delta = time - now
     hours, minutes = delta.seconds / 3600, delta.seconds % 3600 / 60
-    time_from_now = "10 hours 20 minutes from now"
+    time_from_now = "%s hours %s minutes from now" % (hours, minutes)
     return "31 December 2013 at %s (%s)" % (time_str, time_from_now)
     
